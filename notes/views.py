ef11bfdbc7e6f480daa08602da18750e61424eec
AugustineKadima <kadimasam@gmail.com>
2022-08-03 15:24:12 +0300

    Add a class based view

--- notes/views.py
+++ notes/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from .models import Notes
 from django.http import Http404
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, "notes/notes.html", {"notes": all_notes})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name: str="notes/notes_list.html"
 
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404("Note does not exist!")
-    return render(request, "notes/notes_detail.html", {"note":note})
+# def list(request):
+#     all_notes = Notes.objects.all()
+#     return render(request, "notes/notes.html", {"notes": all_notes})
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+
+# def detail(request, pk):
+#     try:
+#         note = Notes.objects.get(pk=pk)
+#     except Notes.DoesNotExist:
+#         raise Http404("Note does not exist!")
+#     return render(request, "notes/notes_detail.html", {"note":note})
